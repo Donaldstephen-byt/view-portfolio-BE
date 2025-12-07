74ba8e7de9a111c401062621c7c338463e5ae46d
Donald stephen <donalduko69@gmail.com>
2025-12-07 09:22:37 +0100

    fix: added new rout to get items

--- learn.py
+++ learn.py
@@ -1,5 +1,12 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import json
+import os
 DATA_FILE = "items.json"
 
+app = FastAPI()
 class Item(BaseModel):
     name: str
 
