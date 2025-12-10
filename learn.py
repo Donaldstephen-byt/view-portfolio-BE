e150afecbb762f910c9222dd090d68b42dff99c6
Donald stephen <donalduko69@gmail.com>
2025-12-10 09:14:43 +0100

    Fix: restored ps for anothe png reform

--- learn.py
+++ learn.py
@@ -26,3 +26,30 @@ def create_item(item: Item):
     items.append(item.dict())
     save_items(items)
     return {"message": "Item saved", "item": item}
+
+
+
+# from turtle import Screen, tracer, color, circle, fd, rt, done
+# from colorsys import hsv_to_rgb
+
+# def generate_turtle_image():
+#     screen = Screen()
+#     screen.setup(width=800, height=600)
+#     screen.bgcolor("black")
+#     tracer(0)  # draw instantly
+#     h = 0
+#     for i in range(225):
+#         r, g, b = hsv_to_rgb(h, 1, 1)
+#         color(int(r*255), int(g*255), int(b*255))
+#         h += 0.01
+#         for j in range(4):
+#             circle(30 + j + 4, -90)
+#             fd(300)
+#             rt(90)
+#             circle(10)
+#             rt(10)
+#     screen.update()
+#     # keep window open
+#     done()
+
+# generate_turtle_image()
